7262055ba709c11a651a46a18d50cb7c7c961d80
bird123 <goldmedal2016@mail.ru>
2023-04-16 15:23:44 +0800

    3 times

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://master--superb-melomakarona-3b142c.netlify.app/' });
+const API = axios.create({ baseURL: 'https://ilyabokh.netlify.app/' });
 
 API.interceptors.request.use((req) => {
 	if (localStorage.getItem('profile')) {
