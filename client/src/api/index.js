a4267470f0a6181e130db674ac810cc9f0a6689b
bird123 <goldmedal2016@mail.ru>
2023-04-23 14:31:42 +0800

    idk

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://master--ilyabokh.netlify.app/' });
+const API = axios.create({ baseURL: 'https://ilyabokh-back.onrender.com/' });
 
 API.interceptors.request.use((req) => {
 	if (localStorage.getItem('profile')) {
